1c191d027e047f4204fa63cdaf7a79ed86747cf5
Adriaan van der Bergh <avanderbergh@gmail.com>
2023-04-06 20:41:04 +0200

    Fix home

--- home/default.nix
+++ home/default.nix
@@ -1,3 +1,11 @@
+{ config, pkgs, ... }:
+
 {
+  home.username = "avanderbergh";
+  home.homeDirectory = "/home/avanderbergh";
+
+  home.stateVersion = "22.11";
+
+  programs.home-manager.enable = true;
   programs.vscode.enable = true;
 }
