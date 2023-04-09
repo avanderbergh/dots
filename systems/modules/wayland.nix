547bc205a986a854fafaa6cdc580567f2f4b5b4e
Adriaan van der Bergh <avanderbergh@gmail.com>
2023-04-09 13:45:17 +0200

    Add validation layer

--- systems/modules/wayland.nix
+++ systems/modules/wayland.nix
@@ -20,22 +20,23 @@ in {
     '';
 
     sessionVariables = {
-      GBM_BACKEND = "nvidia-drm";
       __GL_GSYNC_ALLOWED = "0";
       __GL_VRR_ALLOWED = "0";
-      WLR_DRM_NO_ATOMIC = "1";
       __GLX_VENDOR_LIBRARY_NAME = "nvidia";
       _JAVA_AWT_WM_NONREPARENTING = "1";
-      QT_QPA_PLATFORM = "wayland";
-      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+      GBM_BACKEND = "nvidia-drm";
       GDK_BACKEND = "wayland";
-      WLR_NO_HARDWARE_CURSORS = "1";
       MOZ_ENABLE_WAYLAND = "1";
-      WLR_BACKEND = "vulkan";
-      WLR_RENDERER = "vulkan";
-      XCURSOR_SIZE = "24";
       NIXOS_OZONE_WL = "1";
       PATH = [ "$HOME/.local/bin/:$PATH" ];
+      QT_QPA_PLATFORM = "wayland";
+      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+      VK_INSTANCE_LAYERS = "VK_LAYER_KHRONOS_validation";
+      WLR_BACKEND = "vulkan";
+      WLR_DRM_NO_ATOMIC = "1";
+      WLR_NO_HARDWARE_CURSORS = "1";
+      WLR_RENDERER = "vulkan";
+      XCURSOR_SIZE = "24";
     };
 
     systemPackages = with pkgs; [
