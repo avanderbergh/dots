a068a6ef350f7d0c03de60115e041a4148a98199
Adriaan van der Bergh <avanderbergh@gmail.com>
2023-04-11 23:10:00 +0200

    Add cursor theme

--- home/avanderbergh/features/desktop/default.nix
+++ home/avanderbergh/features/desktop/default.nix
@@ -1,9 +1,9 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   xsession = {
     enable = true;
     windowManager.bspwm = {
       enable = true;
-      monitors = {"eDP-1" = ["code" "web" "chat" "music" "prod"];};
+      monitors = { "eDP-1" = [ "code" "web" "chat" "music" "prod" ]; };
     };
   };
 
@@ -16,9 +16,11 @@
       # Move between windows
       "super + {h,j,k,l}" = "bspc node -f {west,south,north,east}";
       # Swap windows
-      "super + shift + {h,j,k,l}" = "bspc node --swap {west,south,north,east} --follow";
+      "super + shift + {h,j,k,l}" =
+        "bspc node --swap {west,south,north,east} --follow";
       # Resize windows
-      "super + ctrl + {h,j,k,l}" = "bspc node -z {left -20 0,bottom 0 20,top 0 -20,right 20 0}";
+      "super + ctrl + {h,j,k,l}" =
+        "bspc node -z {left -20 0,bottom 0 20,top 0 -20,right 20 0}";
       # Close or kill focused window
       "super + {_,shift + }w" = "bspc node -{c,k}";
     };
@@ -65,9 +67,14 @@
     theme = {
       name = "Catppuccin-Mocha-Standard-Pink-Dark";
       package = pkgs.catppuccin-gtk.override {
-        accents = ["pink"];
+        accents = [ "pink" ];
         variant = "mocha";
       };
+    };
+    cursorTheme = {
+      name = "Catppuccin-Mocha-Dark-Cursors";
+      package = pkgs.catppuccin-cursors.mochaDark;
+      size = 48;
     };
   };
 
