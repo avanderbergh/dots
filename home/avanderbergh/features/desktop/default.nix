8375c4a74aa9e3162eef2e8b3f92a6ec66ea5c46
Adriaan van der Bergh <avanderbergh@gmail.com>
2023-04-11 14:31:18 +0200

    disable polybar

--- home/avanderbergh/features/desktop/default.nix
+++ home/avanderbergh/features/desktop/default.nix
@@ -1,30 +1,30 @@
 { pkgs, ... }: {
   xsession.windowManager.bspwm.enable = true;
 
-  services.polybar = {
-    enable = true;
-    config = {
-      "bar/top" = {
-        monitor = "eDP-1";
-        width = "100%";
-        height = "3%";
-        radius = 0;
-        modules-center = "date";
-      };
-      "module/date" = {
-        type = "internal/date";
-        internal = 5;
-        date = "%d.%m.%y";
-        time = "%H:%M";
-        label = "%time%  %date%";
-      };
-      script = ''
-        # polybar script
-        polybar top &
+  # services.polybar = {
+  #   enable = true;
+  #   config = {
+  #     "bar/top" = {
+  #       monitor = "eDP-1";
+  #       width = "100%";
+  #       height = "3%";
+  #       radius = 0;
+  #       modules-center = "date";
+  #     };
+  #     "module/date" = {
+  #       type = "internal/date";
+  #       internal = 5;
+  #       date = "%d.%m.%y";
+  #       time = "%H:%M";
+  #       label = "%time%  %date%";
+  #     };
+  #     script = ''
+  #       # polybar script
+  #       polybar top &
 
-      '';
-    };
-  };
+  #     '';
+  #   };
+  # };
   programs.rofi.enable = true;
 
   gtk = {
