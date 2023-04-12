65fb02c5c3ab4444873a35e9203a756a5060d994
Adriaan van der Bergh <avanderbergh@gmail.com>
2023-04-12 09:28:59 +0200

    gnome keyring

--- hosts/common/global/security.nix
+++ hosts/common/global/security.nix
@@ -1,20 +1,3 @@
 {pkgs, ...}: {
-  # environment.systemPackages = [pkgs.polkit_gnome];
-
-  # systemd = {
-  #   user.services.polkit-gnome-authentication-agent-1 = {
-  #     description = "polkit-gnome-authentication-agent-1";
-  #     wantedBy = ["graphical-session.target"];
-  #     wants = ["graphical-session.target"];
-  #     after = ["graphical-session.target"];
-  #     serviceConfig = {
-  #       Type = "simple";
-  #       ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-  #       Restart = "on-failure";
-  #       RestartSec = 1;
-  #       TimeoutStopSec = 10;
-  #     };
-  #   };
-  # };
-  services.passSecretService.enable = true;
+  services.gnome.gnome-keyring.enable = true;
 }
