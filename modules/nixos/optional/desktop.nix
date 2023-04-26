3aca2a3529cb6901811a921f73a51b3b03961072
Adriaan van der Bergh <avanderbergh@gmail.com>
2023-04-26 12:53:32 +0400

    Theme the Greeter Fixes #8

--- modules/nixos/optional/desktop.nix
+++ modules/nixos/optional/desktop.nix
@@ -1,7 +1,37 @@
-{ pkgs, ... }: {
+{
+  pkgs,
+  colors,
+  ...
+}: {
   services.xserver = {
     enable = true;
-    displayManager.lightdm.enable = true;
+    displayManager = {
+      lightdm = {
+        enable = true;
+        background = "/usr/share/background.jpg";
+        greeters = {
+          gtk = {
+            enable = true;
+            theme = {
+              name = "Catppuccin-Mocha-Standard-Pink-Dark";
+              package = pkgs.catppuccin-gtk.override {
+                accents = ["pink"];
+                variant = "mocha";
+              };
+            };
+            cursorTheme = {
+              name = "Catppuccin-Mocha-Dark-Cursors";
+              package = pkgs.catppuccin-cursors.mochaDark;
+              size = 48;
+            };
+            extraConfig = ''
+              [greeter]
+              xft-dpi = 261
+            '';
+          };
+        };
+      };
+    };
     windowManager.bspwm.enable = true;
   };
 }
