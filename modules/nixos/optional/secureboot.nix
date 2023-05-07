941898ed87f5b3e19be63032343c923305e96ace
Adriaan van der Bergh <avanderbergh@gmail.com>
2023-05-07 14:37:11 +0200

    Set up secure boot on hermes (#34)
    * Set up secure boot on hermes
    Fixes #33
    
    * Set up secure boot on hermes
    Fixes #33

--- modules/nixos/optional/secureboot.nix
+++ modules/nixos/optional/secureboot.nix
@@ -5,12 +5,18 @@
 }: {
   imports = [inputs.lanzaboote.nixosModules.lanzaboote];
 
+  # Follow directions here: https://github.com/nix-community/lanzaboote/blob/master/docs/QUICK_START.md
+
   boot.bootspec.enable = true;
   boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.lanzaboote = {
     enable = true;
     pkiBundle = "/etc/secureboot";
   };
+
+  # To enable TPM2 support, you need to enroll the TPM2 device first:
+  # sudo systemd-cryptenroll --tpm2-device=list
+  # sudo systemd-cryptenroll --wipe-slot=tpm2 /dev/disk/by-label/luks --tpm2-device=auto --tpm2-pcrs=0+2+7
 
   security.tpm2.enable = true;
   security.tpm2.tctiEnvironment.enable = true;
