61ecdd0239ed9fd254a33b9874c11846083650b6
Adriaan van der Bergh <avanderbergh@gmail.com>
2023-05-15 23:34:23 +0200

    Set up lock screen

--- modules/nixos/optional/desktop.nix
+++ modules/nixos/optional/desktop.nix
@@ -40,5 +40,15 @@
       };
     };
     windowManager.bspwm.enable = true;
+    xautolock = {
+      enable = true;
+      locker = "${pkgs.i3lock-color}/bin/i3lock-color --blur=15";
+      nowlocker = "${pkgs.i3lock-color}/bin/i3lock-color --blur=15";
+      notifier = "${pkgs.libnotify}/bin/notify-send -u critical -t 10000 -- 'Locking screen in 10 seconds'";
+    };
+  };
+  programs.i3lock = {
+    enable = true;
+    package = pkgs.i3lock-color;
   };
 }
