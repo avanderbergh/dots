2aab73f1218a9d3cbf7eb74f201075037b552d75
Adriaan van der Bergh <avanderbergh@gmail.com>
2023-05-15 23:00:35 +0200

    Add focused window border and desktop rules to bspwm

--- modules/hm/desktop/common/bspwm.nix
+++ modules/hm/desktop/common/bspwm.nix
@@ -1,9 +1,34 @@
-{hostConfig, ...}: {
+{
+  hostConfig,
+  colors,
+  ...
+}: {
   xsession = {
     enable = true;
     windowManager.bspwm = {
       enable = true;
       monitors = {"${hostConfig.monitor}" = ["code" "term" "web" "chat" "music" "prod"];};
+      settings = {
+        normal_border_color = "${colors.crust}";
+        focused_border_color = "${colors.mauve}";
+        active_border_color = "${colors.lavender}";
+        gapless_monocle = true;
+        border_width = 3;
+      };
+      rules = {
+        "Alacritty" = {
+          desktop = "term";
+        };
+        "Google-chrome" = {
+          desktop = "web";
+        };
+        "Code" = {
+          desktop = "code";
+        };
+        "Obsidian" = {
+          desktop = "prod";
+        };
+      };
       extraConfig = ''
         bspc config pointer_modifier mod4
         bspc config pointer_action1 move
