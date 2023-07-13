f25b44393285b311aeba4547098d19c8aa17bcd4
Adriaan van der Bergh <avanderbergh@gmail.com>
2023-07-13 12:21:08 +0200

    Update Postgres

--- modules/hm/desktop/default.nix
+++ modules/hm/desktop/default.nix
@@ -13,7 +13,7 @@
   gtk = {
     enable = true;
     theme = {
-      name = "Catppuccin-Mocha-Standard-Pink-Dark";
+      name = "Catppuccin-Mocha-Standard-Pink-dark";
       package = pkgs.catppuccin-gtk.override {
         accents = ["pink"];
         variant = "mocha";
--- modules/nixos/optional/postgres.nix
+++ modules/nixos/optional/postgres.nix
@@ -1,23 +1,41 @@
-{pkgs, ...}: {
-  services.postgresql = {
-    enable = true;
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  services = {
+    postgresql = {
+      enable = true;
 
-    package = pkgs.postgresql_14;
+      package = pkgs.postgresql_14;
 
-    extraPlugins = with pkgs.postgresql_14.pkgs; [
-      pgvector
-    ];
+      extraPlugins = with pkgs.postgresql_14.pkgs; [
+        pgvector
+      ];
 
-    authentication = ''
-      local all   postgres      peer    map=eroot
-      local all   avanderbergh  peer
-      local all   all           md5
-    '';
+      settings = {
+        log_connections = true;
+        log_statement = "all";
+        logging_collector = true;
+        log_disconnections = true;
+        # log_destination = lib.mkForce "syslog";
+      };
 
-    identMap = ''
-      eroot     root      postgres
-      eroot     postgres  postgres
-    '';
+      authentication = ''
+        local all   postgres      peer    map=eroot
+        local all   avanderbergh  peer
+        local all   all           md5
+      '';
+
+      identMap = ''
+        eroot     root      postgres
+        eroot     postgres  postgres
+      '';
+    };
+
+    syslogd.extraConfig = {
+      "local0.*" = "/var/log/postgresql";
+    };
   };
 
   environment.persistence."/persist".directories = ["/var/lib/postgresql"];
