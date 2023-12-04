57c13d7c218dbc5332292afa9bd0d10437a40d44
Adriaan van der Bergh <avanderbergh@gmail.com>
2023-12-04 09:16:42 +0100

    Add ssh to hermes

--- hosts/hermes/default.nix
+++ hosts/hermes/default.nix
@@ -34,14 +34,24 @@
     initrd = {
       availableKernelModules = ["xhci_pci" "nvme" "usb_storage" "sd_mod" "usbhid" "ahci"];
       kernelModules = ["tpm_tis"];
-      luks.devices."enc".device = "/dev/disk/by-label/luks";
+      luks.devices = {
+        "enc".device = "/dev/disk/by-label/luks";
+        "enc_ssd".device = "/dev/disk/by-label/luks_ssd";
+      };
     };
     kernelModules = ["kvm-amd"];
   };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/boot";
-    fsType = "vfat";
+  fileSystems = {
+    "/boot" = {
+      device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+    "/home/avanderbergh/ssd" = {
+      device = "/dev/mapper/enc_ssd";
+      fsType = "btrfs";
+      options = ["compress=zstd" "noatime" "ssd"];
+    };
   };
   swapDevices = [
     {
