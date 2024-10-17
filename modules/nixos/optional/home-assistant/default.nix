550cfceebae026a9911501c13e8ec5f99ad1bc0e
Adriaan van der Bergh <avanderbergh@gmail.com>
2024-10-17 18:19:47 +0200

    Add accuweather component to Home Assistant default configuration

--- modules/nixos/optional/home-assistant/default.nix
+++ modules/nixos/optional/home-assistant/default.nix
@@ -3,19 +3,19 @@
     enable = true;
     openFirewall = true;
     extraComponents = [
+      "accuweather"
       "esphome"
       "ffmpeg"
       "github"
       "hue"
       "met"
       "radio_browser"
-      "spotify"
     ];
     config = {
       # Includes dependencies for a basic setup
       # https://www.home-assistant.io/integrations/default_config/
       default_config = {};
-      feedreader.urls = ["https://nixos.org/blogs.xml"];
     };
+    configWritable = true;
   };
 }
