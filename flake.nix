c6c71ba7049c767984b7f506c9579dedab308ec9
Adriaan van der Bergh <avanderbergh@gmail.com>
2024-10-25 12:04:02 +0200

    Refactor NixOS build workflow to add avanderbergh Cachix cache

--- flake.nix
+++ flake.nix
@@ -29,11 +29,13 @@
     accept-flake-config = true;
 
     extra-substituters = [
+      "https://avanderbergh.cachix.org"
       "https://nix-community.cachix.org"
       "https://cuda-maintainers.cachix.org"
     ];
 
     extra-trusted-public-keys = [
+      "avanderbergh.cachix.org-1:SlceCUs8jcVTNiUBVOjDasw1/E3p/p/hAm32zakyPHU="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     ];
