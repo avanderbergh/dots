90a39658b595ccb62029ccd46196c89919c6f198
Adriaan van der Bergh <avanderbergh@gmail.com>
2024-10-27 17:55:42 +0100

    Refactor power management settings for laptop lid behavior

--- modules/nixos/optional/laptop.nix
+++ modules/nixos/optional/laptop.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   services = {
     libinput.touchpad.clickMethod = "clickfinger";
     logind = {
@@ -7,31 +7,22 @@
     };
     acpid = {
       enable = true;
-      extraRules = ''
-        event=button/lid.*
-        action=/etc/acpi/handle-lid.sh %e
-      '';
+
+      lidEventCommands = {
+        opened = ''
+          export DISPLAY=:0
+          export XAUTHORITY=/home/avanderbergh/.Xauthority
+          su avanderbergh -c "xset dpms force on"
+        '';
+        closed = ''
+          export DISPLAY=:0
+          export XAUTHORITY=/home/avanderbergh/.Xauthority
+          su avanderbergh -c "xset dpms force off"
+        '';
+      };
     };
-
-    # To switch off the display when the lid is closed.
-    environment.etc."acpi/handle-lid.sh".text = ''
-      #!/usr/bin/env bash
-
-      export DISPLAY=:0
-      export XAUTHORITY=/home/avanderbergh/.Xauthority
-
-      # Get the lid state from the event parameter
-      lid_state=$(echo $@ | awk '{print $3}')
-
-      if [ "$lid_state" = "close" ]; then
-        logger "Lid closed: turning off display"
-        su avanderbergh -c "xset dpms force off"
-      elif [ "$lid_state" = "open" ]; then
-        logger "Lid opened: turning on display"
-        su avanderbergh -c "xset dpms force on"
-      fi
-    '';
-
-    environment.etc."acpi/handle-lid.sh".mode = "0755";
   };
+  environment.systemPackages = with pkgs; [
+    xorg.xset
+  ];
 }
