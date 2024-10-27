2e5d063c6433d3cd0b4bbb6c1da923f9d523ea12
Adriaan van der Bergh <avanderbergh@gmail.com>
2024-10-27 22:40:23 +0100

    Move substitutors

--- flake.nix
+++ flake.nix
@@ -30,14 +30,10 @@
 
     extra-substituters = [
       "http://zoidberg.fritz.box:5000"
-      "https://nix-community.cachix.org"
-      "https://cuda-maintainers.cachix.org"
     ];
 
     extra-trusted-public-keys = [
       "zoidberg.fritz.box:TRN+E73yI/YM72iiC7mLrEMCvFMOSqFZh520vIv7KWE="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     ];
   };
 
