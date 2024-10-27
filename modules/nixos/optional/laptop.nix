72d3bfcf82b0d0c1be972e801d6e39d25c598d1e
Adriaan van der Bergh <avanderbergh@gmail.com>
2024-10-27 17:58:36 +0100

    Refactor power management settings for laptop lid behavior

--- modules/nixos/optional/laptop.nix
+++ modules/nixos/optional/laptop.nix
@@ -1,28 +1,6 @@
 {pkgs, ...}: {
   services = {
     libinput.touchpad.clickMethod = "clickfinger";
-    logind = {
-      enable = true;
-      lidSwitch = "ignore";
-    };
-    acpid = {
-      enable = true;
-
-      lidEventCommands = {
-        opened = ''
-          export DISPLAY=:0
-          export XAUTHORITY=/home/avanderbergh/.Xauthority
-          su avanderbergh -c "xset dpms force on"
-        '';
-        closed = ''
-          export DISPLAY=:0
-          export XAUTHORITY=/home/avanderbergh/.Xauthority
-          su avanderbergh -c "xset dpms force off"
-        '';
-      };
-    };
+    services.logind.lidSwitch = "lock";
   };
-  environment.systemPackages = with pkgs; [
-    xorg.xset
-  ];
 }
