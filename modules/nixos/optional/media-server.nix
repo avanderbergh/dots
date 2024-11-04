f51905ee8ebb2d05863dfc36867074cd6f8df8f3
Adriaan van der Bergh <avanderbergh@gmail.com>
2024-11-04 09:26:40 +0100

    Disable blender & jellyfin

--- modules/nixos/optional/media-server.nix
+++ modules/nixos/optional/media-server.nix
@@ -1,6 +1,8 @@
 {
-  services.jellyfin = {
-    enable = true;
-    openFirewall = true;
-  };
+  # https://github.com/NixOS/nixpkgs/issues/353600
+
+  # services.jellyfin = {
+  #   enable = true;
+  #   openFirewall = true;
+  # };
 }
