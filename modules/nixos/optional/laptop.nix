e0db52dd41874e0675e6d5cc62ec4c0dad565772
Adriaan van der Bergh <avanderbergh@gmail.com>
2024-12-04 10:26:21 +0100

    Enhance touchpad settings in laptop configuration for improved usability

--- modules/nixos/optional/laptop.nix
+++ modules/nixos/optional/laptop.nix
@@ -1,6 +1,10 @@
 {pkgs, ...}: {
   services = {
-    libinput.touchpad.clickMethod = "clickfinger";
+    libinput.touchpad = {
+      clickMethod = "clickfinger";
+      disableWhileTyping = true;
+      sendEventsMode = "disabled-on-external-mouse";
+    };
     logind.lidSwitch = "lock";
   };
 }
