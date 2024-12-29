a26a99728a80dd190bf7b35c2596d32f5ce76c26
Adriaan van der Bergh <avanderbergh@gmail.com>
2024-12-29 13:50:32 +0100

    Add autorandr profiles for home and mobile setups, enable fingerprint authentication, and adjust DPI settings

--- modules/nixos/optional/fingerprint.nix
+++ modules/nixos/optional/fingerprint.nix
@@ -7,5 +7,16 @@
     };
   };
 
+  security.pam.services = {
+    login.fprintAuth = true;
+    i3lock = {
+      fprintAuth = true;
+      text = ''
+        auth sufficient pam_unix.so try_first_pass likeauth nullok
+        auth sufficient pam_fprintd.so
+      '';
+    };
+  };
+
   environment.persistence."/persist".directories = ["/var/lib/fprint"];
 }
--- modules/nixos/optional/hidpi.nix
+++ modules/nixos/optional/hidpi.nix
@@ -1,6 +1,5 @@
 {pkgs, ...}: {
   console.font = "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
-  services.xserver.dpi = 192;
 
   fonts.fontconfig = {
     antialias = false;
