6cf9f651d6fcc0db5bd786321f73ed64bb11f05c
Adriaan van der Bergh <avanderbergh@gmail.com>
2025-04-14 09:44:55 +0200

    Refactor Node.js package configuration to use latest versions and remove deprecated packages

--- modules/hm/node.nix
+++ modules/hm/node.nix
@@ -1,21 +1,12 @@
 {pkgs, ...}: {
   home = {
     packages = with pkgs; [
-      nodejs_20
-      nodePackages.node-gyp-build
-      nodePackages.pnpm
-      nodePackages.zx
-      playwright-driver
-
-      # For node-gyp-build
-      gnat
-      gnumake
+      nodePackages_latest.nodejs
+      nodePackages_latest.node-gyp-build
+      pnpm
+      zx
+      playwright
+      playwright-test
     ];
-
-    sessionVariables = {
-      PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
-      # TODO: This find a way to update the path to the latest version
-      PUPPETEER_EXECUTABLE_PATH = "${pkgs.playwright-driver.browsers}/chromium-1080/chrome-linux/chrome";
-    };
   };
 }
