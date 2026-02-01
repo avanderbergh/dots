8e9e9c626be9a854d5f802082a62f3747233fc49
Adriaan van der Bergh <avanderbergh@gmail.com>
2026-02-01 12:24:39 +0100

    Add systemd tmpfiles rules for /bin directory

--- modules/nixos/global/default.nix
+++ modules/nixos/global/default.nix
@@ -55,6 +55,10 @@
 
   services.udisks2.enable = true;
 
+  systemd.tmpfiles.rules = [
+    "L+ /bin - - - - /run/current-system/sw/bin"
+  ];
+
   environment = {
     pathsToLink = ["/bin"];
     systemPackages = with pkgs; [
