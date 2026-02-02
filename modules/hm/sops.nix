1b91d705eccf020e7f9eb98ec1fad935eb178ef2
Adriaan van der Bergh <avanderbergh@gmail.com>
2026-02-02 22:33:48 +0100

    SOPS_KEY_CMD to get age key for sops editing

--- modules/hm/sops.nix
+++ modules/hm/sops.nix
@@ -1,6 +1,15 @@
-{
+{pkgs, ...}: {
   sops = {
     defaultSopsFile = ../../secrets/secrets.yaml;
     age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+  };
+
+  home = {
+    sessionVariables = {
+      SOPS_AGE_KEY_CMD = "${pkgs.ssh-to-age}/bin/ssh-to-age -private-key -i /etc/ssh/ssh_host_ed25519_key";
+    };
+    packages = [
+      pkgs.ssh-to-age
+    ];
   };
 }
