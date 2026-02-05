cc2f9a52f35f37aaf8fab763acc1b288561b0426
Adriaan van der Bergh <avanderbergh@gmail.com>
2026-02-05 22:32:13 +0100

    Refactor build-tools.nix to include additional build dependencies and improve session variable configuration

--- modules/hm/bot/build-tools.nix
+++ modules/hm/bot/build-tools.nix
@@ -1,7 +1,46 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    gcc
-    gnumake
-    pkg-config
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  buildDeps = with pkgs; [
+    bzip2
+    libffi
+    ncurses
+    openssl
+    readline
+    sqlite
+    xz
+    zlib
   ];
+
+  devPkgs = map lib.getDev buildDeps;
+
+  includeFlags = lib.concatStringsSep " " (map (p: "-I${lib.getDev p}/include") buildDeps);
+  libFlags = lib.concatStringsSep " " (map (p: "-L${lib.getLib p}/lib") buildDeps);
+  rpathFlags = lib.concatStringsSep " " (map (p: "-Wl,-rpath,${lib.getLib p}/lib") buildDeps);
+in {
+  home = {
+    packages =
+      (with pkgs; [
+        curl
+        gcc
+        gnumake
+        gnupg
+        pkg-config
+        python3
+      ])
+      ++ buildDeps;
+
+    sessionVariables = {
+      # Helps mise/asdf plugins compile toolchains on NixOS (pyenv/python-build, node-build, etc.).
+      CFLAGS = includeFlags;
+      CPPFLAGS = includeFlags;
+      LDFLAGS = "${libFlags} ${rpathFlags}";
+      PKG_CONFIG_PATH = lib.concatStringsSep ":" [
+        (lib.makeSearchPath "lib/pkgconfig" devPkgs)
+        (lib.makeSearchPath "share/pkgconfig" devPkgs)
+      ];
+    };
+  };
 }
