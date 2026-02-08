b0f792831fa7feac5331a70ee3d347ea028ae26a
morbo <morbo@flipfish.net>
2026-02-08 16:08:07 +0100

    pkgs: switch pi-coding-agent back to lightweight npx wrapper

--- pkgs/pi-coding-agent/default.nix
+++ pkgs/pi-coding-agent/default.nix
@@ -1,29 +1,11 @@
 {
-  lib,
-  buildNpmPackage,
-  fetchzip,
+  writeShellApplication,
+  nodejs_24,
 }:
-buildNpmPackage (finalAttrs: {
-  pname = "pi-coding-agent";
-  version = "0.52.8";
-
-  src = fetchzip {
-    url = "https://registry.npmjs.org/@mariozechner/pi-coding-agent/-/pi-coding-agent-${finalAttrs.version}.tgz";
-    hash = "sha256-O2O/a28S4Qogue1iGSOthZi0VmofTzAq3R5k0SaPSN0=";
-  };
-
-  postPatch = ''
-    cp ${./package-lock.json} package-lock.json
+writeShellApplication {
+  name = "pi";
+  runtimeInputs = [nodejs_24];
+  text = ''
+    exec npx --yes @mariozechner/pi-coding-agent "$@"
   '';
-
-  npmDepsHash = "sha256-yS+6/SSp8yGqncOskpe1PMxSorLgX6A6cn2GjVjGPNw=";
-  dontNpmBuild = true;
-
-  meta = with lib; {
-    description = "pi.dev coding agent CLI";
-    homepage = "https://pi.dev";
-    license = licenses.mit;
-    mainProgram = "pi";
-    platforms = platforms.unix;
-  };
-})
+}
