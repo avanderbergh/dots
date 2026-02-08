fb9908cf1dc50b39fbf35c341a2d3900d138d6ad
morbo <morbo@flipfish.net>
2026-02-08 15:30:44 +0100

    pkgs: make pi-coding-agent reproducible with pinned npm deps

--- pkgs/pi-coding-agent/default.nix
+++ pkgs/pi-coding-agent/default.nix
@@ -1,11 +1,29 @@
 {
-  writeShellApplication,
-  nodejs_24,
+  lib,
+  buildNpmPackage,
+  fetchurl,
 }:
-writeShellApplication {
-  name = "pi";
-  runtimeInputs = [nodejs_24];
-  text = ''
-    exec npx --yes @mariozechner/pi-coding-agent "$@"
+buildNpmPackage rec {
+  pname = "pi-coding-agent";
+  version = "0.52.8";
+
+  src = fetchurl {
+    url = "https://registry.npmjs.org/@mariozechner/pi-coding-agent/-/pi-coding-agent-${version}.tgz";
+    hash = "sha256-zw4R20/TQztVIMKc52PHTcyXLRTdYAuuaDOn1WC9q3Y=";
+  };
+
+  postPatch = ''
+    cp ${./package-lock.json} package-lock.json
   '';
+
+  npmDepsHash = "sha256-yS+6/SSp8yGqncOskpe1PMxSorLgX6A6cn2GjVjGPNw=";
+  dontNpmBuild = true;
+
+  meta = with lib; {
+    description = "pi.dev coding agent CLI";
+    homepage = "https://pi.dev";
+    license = licenses.mit;
+    mainProgram = "pi";
+    platforms = platforms.unix;
+  };
 }
