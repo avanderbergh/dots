969658872265a98c9fa333a8161a5120063d765b
morbo <morbo@flipfish.net>
2026-02-08 18:10:38 +0100

    hermes: add cloudflared tunnel for ssh.hermes.adriaan.cc

---
+++ modules/nixos/optional/cloudflared.nix
@@ -0,0 +1,25 @@
+{
+  ...
+}: {
+  services.cloudflared = {
+    enable = true;
+    tunnels = {
+      "e0b6e7f2-c209-4f32-b371-669a635695e1" = {
+        credentialsFile = "/run/secrets/cloudflared-hermes-credentials";
+        default = "http_status:404";
+        ingress = {
+          "ssh.hermes.adriaan.cc" = {
+            service = "ssh://127.0.0.1:22";
+          };
+        };
+      };
+    };
+  };
+
+  sops.secrets.cloudflared-hermes-credentials = {
+    key = "cloudflared/hermes/credentials";
+    owner = "cloudflared";
+    group = "cloudflared";
+    mode = "0400";
+  };
+}
