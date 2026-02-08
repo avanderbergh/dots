d9629115bf0dc240f5aafb239aab0ea198cd4c52
morbo <morbo@flipfish.net>
2026-02-08 21:31:12 +0100

    openssh: add ECDSA host key for browser SSH compatibility

--- modules/nixos/global/openssh.nix
+++ modules/nixos/global/openssh.nix
@@ -33,6 +33,10 @@ in {
         path = "${lib.optionalString hasOptinPersistence "/persist"}/etc/ssh/ssh_host_ed25519_key";
         type = "ed25519";
       }
+      {
+        path = "${lib.optionalString hasOptinPersistence "/persist"}/etc/ssh/ssh_host_ecdsa_key";
+        type = "ecdsa";
+      }
     ];
   };
 
