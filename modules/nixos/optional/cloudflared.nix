cc7d330cf433068e1d5f1682429ca8fe89e8ffba
morbo <morbo@flipfish.net>
2026-02-08 18:49:32 +0100

    cloudflared: fix sops secret ownership for tunnel credentials

--- modules/nixos/optional/cloudflared.nix
+++ modules/nixos/optional/cloudflared.nix
@@ -1,8 +1,11 @@
-{...}: {
+{lib, ...}: let
+  tunnelId = "e0b6e7f2-c209-4f32-b371-669a635695e1";
+  tunnelUnit = "cloudflared-tunnel-${tunnelId}";
+in {
   services.cloudflared = {
     enable = true;
     tunnels = {
-      "e0b6e7f2-c209-4f32-b371-669a635695e1" = {
+      "${tunnelId}" = {
         credentialsFile = "/run/secrets/cloudflared-hermes-credentials";
         default = "http_status:404";
         ingress = {
@@ -14,10 +17,17 @@
     };
   };
 
+  # cloudflared tunnel units use DynamicUser by default; make this tunnel run as
+  # root so it can read a root-owned 0400 credentials file from /run/secrets.
+  systemd.services."${tunnelUnit}".serviceConfig = {
+    DynamicUser = lib.mkForce false;
+    User = lib.mkForce "root";
+  };
+
   sops.secrets.cloudflared-hermes-credentials = {
     key = "cloudflared/hermes/credentials";
-    owner = "cloudflared";
-    group = "cloudflared";
+    owner = "root";
+    group = "root";
     mode = "0400";
   };
 }
