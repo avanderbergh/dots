3775ab9e94aa34697cb27b916386e5dd5419e43e
Morbo <avanderbergh+morbo@gmail.com>
2026-02-09 20:38:50 +0100

    openssh: move Cloudflare CA/principals out of /etc/static for sshd strict mode (#103)
    Co-authored-by: morbo <morbo@flipfish.net>

--- modules/nixos/optional/cloudflare-ssh-ca.nix
+++ modules/nixos/optional/cloudflare-ssh-ca.nix
@@ -1,18 +1,26 @@
-{
+_: {
   services.openssh.settings = {
     PubkeyAuthentication = true;
-    TrustedUserCAKeys = "/etc/ssh/cloudflare-ca.pub";
-    AuthorizedPrincipalsFile = "/etc/ssh/auth_principals/%u";
+    TrustedUserCAKeys = "/var/lib/ssh-ca/cloudflare-ca.pub";
+    AuthorizedPrincipalsFile = "/var/lib/ssh-ca/auth_principals/%u";
   };
 
-  environment.etc = {
-    "ssh/cloudflare-ca.pub".text = ''
-      ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBOzn7HAZE/Kq/O3pTUlvIZHUI4NV47iKW+rlbfZfCzxY24cN4AAEFFBdV1RuG/Zi/6SKoVnvtBjElI1hT5buHFY= open-ssh-ca@cloudflareaccess.org
-    '';
+  system.activationScripts.cloudflareSshCa = {
+    text = ''
+      mkdir -p /var/lib/ssh-ca/auth_principals
+      chmod 0755 /var/lib/ssh-ca
+      chmod 0755 /var/lib/ssh-ca/auth_principals
 
-    "ssh/auth_principals/avanderbergh".text = ''
+      cat > /var/lib/ssh-ca/cloudflare-ca.pub <<'EOF'
+      ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBOzn7HAZE/Kq/O3pTUlvIZHUI4NV47iKW+rlbfZfCzxY24cN4AAEFFBdV1RuG/Zi/6SKoVnvtBjElI1hT5buHFY= open-ssh-ca@cloudflareaccess.org
+      EOF
+      chmod 0644 /var/lib/ssh-ca/cloudflare-ca.pub
+
+      cat > /var/lib/ssh-ca/auth_principals/avanderbergh <<'EOF'
       avanderbergh
       adriaan
+      EOF
+      chmod 0644 /var/lib/ssh-ca/auth_principals/avanderbergh
     '';
   };
 }
