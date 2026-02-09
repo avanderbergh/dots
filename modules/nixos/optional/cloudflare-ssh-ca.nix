5898ccbf18ccdc3439422b7f47add5048831e66f
Adriaan van der Bergh <avanderbergh@gmail.com>
2026-02-09 19:54:04 +0100

    update cloudflare ca cert

--- modules/nixos/optional/cloudflare-ssh-ca.nix
+++ modules/nixos/optional/cloudflare-ssh-ca.nix
@@ -1,12 +1,13 @@
-_: {
+{
   services.openssh.settings = {
+    PubkeyAuthentication = true;
     TrustedUserCAKeys = "/etc/ssh/cloudflare-ca.pub";
     AuthorizedPrincipalsFile = "/etc/ssh/auth_principals/%u";
   };
 
   environment.etc = {
     "ssh/cloudflare-ca.pub".text = ''
-      ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBOoH1YnxoMoB3xrNFtEQViG371+sc1VCNHmumB6D+QvvX9Cnrzu2zMg/FO9AvXd28LlKPUeC3Fhshuq9BfvCa+0= open-ssh-ca@cloudflareaccess.org
+      ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBOzn7HAZE/Kq/O3pTUlvIZHUI4NV47iKW+rlbfZfCzxY24cN4AAEFFBdV1RuG/Zi/6SKoVnvtBjElI1hT5buHFY= open-ssh-ca@cloudflareaccess.org
     '';
 
     "ssh/auth_principals/avanderbergh".text = ''
